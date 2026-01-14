3957a0bcebab8d73299eb392f4a07fb79ae6e219
JoelCaquene <joelcaquenejr@gmail.com>
2026-01-14 08:51:56 +0100

    atualizar imagem v2

--- totalenergies/urls.py
+++ totalenergies/urls.py
@@ -9,13 +9,8 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-# Esta configuração permite que o Render sirva os comprovativos
-# mesmo com DEBUG=False
+# Configuração para servir Media e Static em Produção (Render)
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
-
-# Mantém a compatibilidade com arquivos estáticos em desenvolvimento
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]~
